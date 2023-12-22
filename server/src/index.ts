c5a7811676310837d32773eaf281b57ede702f98
Bennett Smrdel <fireknights318@gmail.com>
2023-12-22 14:42:13 -0600

    feat: Add ts-node

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hey');
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = 8080;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
